ec6ee5a62ae5f1a7207c1023b17eaa8449aba9c1
Renato Crivano <renato.crivano@gmail.com>
2024-11-08 16:33:35 -0300

    default validation message

--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -12,6 +12,6 @@ export const systems = systemsList.map(system => ({
     system,
     wsdl: process.env[`${system}_MNI_WSDL_URL`] as string,
     endpoint: process.env[`${system}_MNI_ENDPOINT_URL`] as string,
-    validation: process.env[`${system}_VALIDATION_MESSAGE`] as string,
+    validation: process.env[`${system}_VALIDATION_MESSAGE`] as string || 'Processo não encontrado',
     hashable: process.env?.[`${system}_HASH_PASSWORD`] === 'true'
 } as System)) ?? new Array<System>
