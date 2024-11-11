0b6b17020f84e99535eae8d538b93e6df4b07ae1
Victor Raton <victor.raton@globalhitss.com.br>
2024-11-11 18:15:56 -0300

    chore: ajustando dependências, compilações e debug

--- next.config.js
+++ next.config.js
@@ -13,19 +13,38 @@ const nextConfig = {
     // trailingSlash: true,
     output: "standalone",
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-            config.module.rules.push({
-                test: /\.(txt|md)$/,
-                // This is the asset module.
-                type: 'asset/source',
-            })
-            return config
+        config.module.rules.push({
+            test: /\.(txt|md)$/,
+            // This is the asset module.
+            type: 'asset/source',
+        })
+        if (isServer) {
+            config.externals = [
+                ...config.externals,
+                {
+                    // Possible drivers for knex - we'll ignore them
+                    // comment the one YOU WANT to use
+                    sqlite3: 'sqlite3',
+                    // mysql2: 'mysql2', // << using this one
+                    mariasql: 'mariasql',
+                    mysql: 'mysql',
+                    mssql: 'mssql',
+                    oracle: 'oracle',
+                    'strong-oracle': 'strong-oracle',
+                    oracledb: 'oracledb',
+                    pg: 'pg',
+                    'pg-query-stream': 'pg-query-stream',
+                }
+            ]
         }
-        // experimental: {
-        //     staleTimes: {
-        //         dynamic: 0,
-        //         static: 180,
-        //     },
-        // },
+        return config
+    }
+    // experimental: {
+    //     staleTimes: {
+    //         dynamic: 0,
+    //         static: 180,
+    //     },
+    // },
 }
 
 module.exports = nextConfig
