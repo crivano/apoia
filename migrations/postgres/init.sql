16dd3700321ee97ac7e02f061a45a2ab7c950d34
Renato Crivano <renato.crivano@gmail.com>
2024-11-27 15:25:33 -0300

    fix: groq novo llama

--- migrations/postgres/init.sql
+++ migrations/postgres/init.sql
@@ -218,3 +218,11 @@ ALTER TABLE ia_document
 ADD COLUMN assigned_category VARCHAR(64) NULL DEFAULT NULL,
 ADD COLUMN predicted_category VARCHAR(64) NULL DEFAULT NULL,
 ALTER COLUMN code TYPE VARCHAR(64);
+
+/* 004 */
+
+/* set schema 'apoia' */;
+
+-- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
+INSERT INTO ia_model(NAME, created_at) VALUES 
+    ('llama-3.1-70b-versatile','2024-11-27 13:38:21');
---
+++ migrations/postgres/migration-004.sql
@@ -0,0 +1,5 @@
+/* set schema 'apoia' */;
+
+-- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
+INSERT INTO ia_model(NAME, created_at) VALUES 
+    ('llama-3.1-70b-versatile','2024-11-27 13:38:21');
