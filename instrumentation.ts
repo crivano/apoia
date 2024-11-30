fe85d646092da5bc6c847684706358ec35de5b20
Victor Leite <victor.leite@adition.com>
2024-11-30 16:17:45 +0000

    disabilitar eureka client

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         if (!process.env.APP_CODE || !process.env.APP_HOST || !process.env.APP_PORT || !process.env.APP_REGISTRY_HOST || !process.env.APP_REGISTRY_PORT) return
-        await import('lib/eureka')
+        //await import('lib/eureka')
     }
 }
