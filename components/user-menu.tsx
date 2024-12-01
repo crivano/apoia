4c9b9f4989329943c80694443419ab13e6f22877
Renato Crivano <renato.crivano@gmail.com>
2024-11-30 21:45:02 -0300

    feat: prefs sem login

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -13,25 +13,33 @@ import { NavigationLink } from './NavigationLink';
 export default async function UserMenu() {
     noStore()
     const session = await getServerSession(authOptions);
-    if (!session) return <><Link href="/auth/signin">Login</Link></>
+    // if (!session) return <NavItem>
+    //     <NavigationLink href="/auth/signin" text="Login" />
+    // </NavItem>
 
     const byCookie = getPrefs()
     const model = byCookie?.model
 
-    const user = session.user
+    const user = session?.user
     return (
         <ul className="navbar-nav me-1 mb-2x mb-lg-0x">
-            {((process.env.ACCESS_ARENA || '').split(';').includes(user.name) || user?.roles?.includes('apoia-role-arena')) &&
+            {((process.env.ACCESS_ARENA || '').split(';').includes(user?.name) || user?.roles?.includes('apoia-role-arena')) &&
                 (<NavItem>
                     <NavigationLink href="/arena" text="Arena" />
                 </NavItem>)}
             <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {user.name}{user.image?.system ? `/${user.image?.system}` : '/PDPJ'}
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {user
+                    ?
+                    <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {user?.name}{user?.image?.system ? `/${user?.image?.system}` : '/PDPJ'}
+                    </a>
+                    : <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Configurações
+                    </a>}
+                <ul className="dropdown-menu  dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    {!user && <li><Link className="dropdown-item" href="/auth/signin">Login</Link></li>}
                     <li><Link className="dropdown-item" href="/prefs">Modelo de IA{model && ` (${model})`}</Link></li>
-                    <li><UserMenuSignout /></li>
+                    {user && <li><UserMenuSignout /></li>}
                 </ul>
             </li>
         </ul>
