d44ffe339d4957accffd52f9cadbe778e6d56241
Renato Crivano <renato.crivano@gmail.com>
2025-02-05 18:32:58 -0300

    deepseek reasoner

--- migrations/mysql/migration-006.sql
+++ migrations/mysql/migration-006.sql
@@ -2,3 +2,5 @@ use apoia;
 
 INSERT INTO ia_model VALUES 
     (8,'deepseek-chat','2025-01-23 12:25:21');
+INSERT INTO ia_model VALUES 
+    (9,'deepseek-reasoner','2025-02-05 12:25:21');
--- migrations/postgres/migration-006.sql
+++ migrations/postgres/migration-006.sql
@@ -1,5 +1,7 @@
 /* set schema 'apoia' */;
 
--- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
 INSERT INTO ia_model(NAME, created_at) VALUES 
     ('deepseek-chat','2025-01-23 13:38:21');
+
+INSERT INTO ia_model(NAME, created_at) VALUES 
+    ('deepseek-reasoner','2025-02-05 13:38:21');
