aafeb89c9e15df13974795ecc193edbf5f6a65c1
Renato Crivano <renato.crivano@gmail.com>
2025-04-02 15:15:30 -0300

    feat: refactor Swagger UI integration by replacing swagger-ui-react with custom SwaggerUI component and updating package dependencies

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import 'swagger-ui-react/swagger-ui.css'
-import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui/dist/swagger-ui.css'
+import SwaggerUI from './swaggerui'
 
 
 type Props = {
