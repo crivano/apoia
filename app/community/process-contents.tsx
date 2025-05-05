e477036936e2ca82ec7e78462125d1250b075f42
Renato Crivano <renato.crivano@gmail.com>
2025-05-05 17:40:33 -0300

    fix: update onSave handler in ChoosePieces to reset requests before changing selected pieces

--- app/community/process-contents.tsx
+++ app/community/process-contents.tsx
@@ -148,7 +148,7 @@ export default function ProcessContents({ prompt, dadosDoProcesso, pieceContent,
     return <div>
         <Subtitulo dadosDoProcesso={dadosDoProcesso} />
         {children}
-        <ChoosePieces allPieces={dadosDoProcesso.pecas} selectedPieces={selectedPieces} onSave={changeSelectedPieces} onStartEditing={() => { setChoosingPieces(true) }} onEndEditing={() => { setRequests([]); setChoosingPieces(false) }} dossierNumber={dadosDoProcesso.numeroDoProcesso} />
+        <ChoosePieces allPieces={dadosDoProcesso.pecas} selectedPieces={selectedPieces} onSave={(pieces) => { setRequests([]); changeSelectedPieces(pieces) }} onStartEditing={() => { setChoosingPieces(true) }} onEndEditing={() => setChoosingPieces(false)} dossierNumber={dadosDoProcesso.numeroDoProcesso} />
         <LoadingPieces />
         <ErrorMsg dadosDoProcesso={dadosDoProcesso} />
         <div className="mb-4"></div>
